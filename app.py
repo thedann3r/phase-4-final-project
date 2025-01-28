2ade648b7a6a9bc301fcac17a8ccd26eb63a9e18
dann <austindan168@gmail.com>
2025-01-28 16:35:34 +0300

    created resource file

--- app.py
+++ app.py
@@ -50,46 +50,6 @@ class PlanesOwners(db.Model, SerializerMixin):
 @app.route('/')
 def home():
     return '<h1>Welcome to the airplane web page!</h1>'
-
-class PlaneCompanyResource(Resource):
-    def get(self):
-        companies = PlaneCompany.query.all()
-        return [company.to_dict() for company in companies]
-    
-    def post(self):
-        data = request.get_json()
-        if not data or not all(key in data for key in ('name', 'founded')):
-            return {'error' : 'missing required fields!'}, 422
-        new_company = PlaneCompany(**data)
-        db.session.add(new_company)
-        db.session.commit()
-        return new_company.to_dict(), 201
-    
-class PlaneCompanyResourceList(Resource):
-    def get(self, id):
-        company = PlaneCompany.filter_by(id=id).first()
-
-        if not company:
-            return {'error' : 'Company was not found!'}, 404
-        return company.to_dict(), 200
-    def patch(self, id):
-        data = request.get_json()
-        company = PlaneCompany.filter_by(id=id).first()
-        if not company:
-            return {'error' : 'Company not found!'}, 404
-        if 'name' in data:
-            company.name = data['name']
-        if 'founded' in data:
-            company.founded = data['founded']
-        db.session.commt()
-        return company.to_dict(), 200
-    def delete(self, id):
-        company = PlaneCompany.query.get(id)
-        if not company:
-            return {'error' : 'Company not found!'}, 404
-        db.session.delete(company)
-        db.session.commit()
-        return {'message' : 'company deleted successfully!'}, 200
     
 api.add_resource(PlaneCompanyResource, '/company')
 api.add_resource(PlaneCompanyResourceList, '/company/<int:id>')
---
+++ resources/airplanes.py
@@ -0,0 +1,43 @@
+from flask_restful import Resource
+from flask import request
+from app import PlaneCompany, Planes,PlanesOwners,Owners,db
+
+class PlaneCompanyResource(Resource):
+    def get(self):
+        companies = PlaneCompany.query.all()
+        return [company.to_dict() for company in companies]
+    
+    def post(self):
+        data = request.get_json()
+        if not data or not all(key in data for key in ('name', 'founded')):
+            return {'error' : 'missing required fields!'}, 422
+        new_company = PlaneCompany(**data)
+        db.session.add(new_company)
+        db.session.commit()
+        return new_company.to_dict(), 201
+    
+class PlaneCompanyResourceList(Resource):
+    def get(self, id):
+        company = PlaneCompany.filter_by(id=id).first()
+
+        if not company:
+            return {'error' : 'Company was not found!'}, 404
+        return company.to_dict(), 200
+    def patch(self, id):
+        data = request.get_json()
+        company = PlaneCompany.filter_by(id=id).first()
+        if not company:
+            return {'error' : 'Company not found!'}, 404
+        if 'name' in data:
+            company.name = data['name']
+        if 'founded' in data:
+            company.founded = data['founded']
+        db.session.commt()
+        return company.to_dict(), 200
+    def delete(self, id):
+        company = PlaneCompany.query.get(id)
+        if not company:
+            return {'error' : 'Company not found!'}, 404
+        db.session.delete(company)
+        db.session.commit()
+        return {'message' : 'company deleted successfully!'}, 200
