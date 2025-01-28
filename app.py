220512661b0a690157cd8d9c10ff4c459e22c81f
dann <austindan168@gmail.com>
2025-01-28 11:12:37 +0300

    table relationshps

--- app.py
+++ app.py
@@ -23,16 +23,35 @@ class PlaneCompany(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False, unique = True)
     founded = db.Column(db.String, nullable = False)
+    planes = db.relationship('Planes', back_populates = 'plane_company')
 
 class Planes(db.Model, SerializerMixin):
     __tablename__ = 'planes'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False, unique = True)
+    planeCompany_id = db.Column(db.Integer, db.ForeignKey('plane_company.id'), nullable = False)
+
+    plane_company = db.relationship('PlaneCompany', back_populates = 'planes')
+    owners = db.relationship('Owners', back_populates = 'planes')
 
 class Owners(db.Model, SerializerMixin):
     __tablename__ = 'owners'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String, nullable = False, unique = True)
+
+    planes = db.relationship('Planes', back_populates = 'owners')
+
+class PlanesOwners(db.Model, SerializerMixin):
+    __tablename__ = 'planesowners'
+    id = db.Column(db.Integer, primary_key = True)
+    
+    planes_id = db.Column(db.Integer, db.ForeignKey('planes.id'), nullable = False)
+    owners_id = db.Column(db.Integer, db.ForeignKey('owners.id'), nullable = False)
+    
+    planes = db.relationship('Planes', back_populates = 'owners')
+    owners = db.relationship('Owners', back_populates = 'planes')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
